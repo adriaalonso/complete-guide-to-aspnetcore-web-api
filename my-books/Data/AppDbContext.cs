d12c6f568cbe9b52b48c9af062336c5334a82402
Adria Alonso Salvat <aalonso@caf.es>
2022-05-30 18:23:07 +0200

    This is our first commit

---
+++ my-books/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
